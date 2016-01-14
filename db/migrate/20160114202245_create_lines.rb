6032d3c7dd9b3d7bb8775ca9a4cdc62ba5d27042
Josh Silverman <joshs.silverman@gmail.com>
2016-01-14 18:20:28 -0500

    add line number storage to lines table

--- db/migrate/20160114202245_create_lines.rb
+++ db/migrate/20160114202245_create_lines.rb
@@ -2,6 +2,7 @@ class CreateLines < ActiveRecord::Migration
   def change
     create_table :lines do |t|
       t.string :text
+      t.integer :number
       t.references :document, index: true, foreign_key: true
 
       t.timestamps null: false
