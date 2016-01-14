27b8247ea9ccb16cfcde0b78e4e8e6dad390c26f
Josh Silverman <joshs.silverman@gmail.com>
2016-01-14 15:23:33 -0500

    add document and lines

---
+++ db/migrate/20160114202245_create_lines.rb
@@ -0,0 +1,10 @@
+class CreateLines < ActiveRecord::Migration
+  def change
+    create_table :lines do |t|
+      t.string :text
+      t.references :document, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
