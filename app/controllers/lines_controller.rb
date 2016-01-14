29248e4f39410fe89635a62a6dbc34b0fd773f96
Josh Silverman <joshs.silverman@gmail.com>
2016-01-14 18:20:57 -0500

    show action for lines

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,2 +1,15 @@
 class LinesController < ApplicationController
+  before_action :set_line, only: [:show]
+
+  # GET /lines/1
+  # GET /lines/1.json
+  def show
+    render json: @line
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_line
+      @line = Line.find(params[:id])
+    end
 end
