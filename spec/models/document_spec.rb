6b35280c829000782e42339cdfc9d3a660111d88
Josh Silverman <joshs.silverman@gmail.com>
2016-01-14 20:45:52 -0500

    add preprocess specs

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  it "is findable" do
-    filename = 'example filename'
-    d = Document.find_or_create_by(filename: filename)
-    expect(Document.count).to   eq(1)
+  let(:filename) { 'spec/fixtures/sample-2lines.txt' }
+  let(:document) { Document.create(filename: filename) }
+
+  describe "#preprocess" do
+    it "creates 2 lines for document" do
+      document.preprocess
+      expect(document.lines.count).to eq(2)
+    end
+
+    it "wont recreate lines if reprocessed" do
+      document.preprocess
+      document.preprocess
+      expect(document.lines.count).to eq(2)
+    end
+
+    it "properly creates foreign keys (for sql version)" do
+      document.preprocess
+      # strange rspec-ism: I had to put #all in there
+      expect(document.lines.all.first.document_id).to eq(document.id)
+    end
   end
 end
